7c05a1c20ce0dbd8b22d3303b606110181c5c8ef
BD <107256942+BDgithu@users.noreply.github.com>
2025-03-26 19:31:43 +0200

    Add files via upload

---
+++ Lesson 4.py
@@ -0,0 +1,58 @@
+# day = input("Який сьогодні день тижня? ")
+
+# if day == "субота": # 1
+  
+#     print("Ура! Можна відпочивати!")
+
+# elif day == "mon":
+#     print("mon")
+# elif day == "thu":
+#     print("thu")
+# elif day == "wed":
+#     print("wed")
+# else: # в усіх інших випадках
+#     print("Працюємо")
+# a = 10
+# b = 100
+# print(a < b)
+# print(a > b)
+# print(a <= b)
+# print(a >= b)
+# print(a == b)
+# print(a != b)
+
+# 1
+# system_password = "1983"
+# user_input = input("Enter your password:> ") or "13234"
+
+# if user_input == system_password: # якщо
+#     print("Доступ дозволено")
+# else:# else if (інакше якщо)
+#     print("Доступ заборонено")
+    
+# print(user_input)
+
+# # 2
+# current_temp = 21
+# # and, or, not
+# if current_temp >= 20:
+#     print("Wear light clothes")
+# if 10 <= current_temp:
+#     print("Wear mid clothes")
+# if current_temp < 10:
+#     print("Wear warm clothes")
+
+# 3
+
+# user_act = input("Cose 'left', 'rorward' or 'right' (1, 2, 3):> ")
+
+# if user_act == "1":
+#     print("Гравець знаходить скарб (великий коштовний камінь)")
+# elif user_act == "2":
+#     print(" Гравець знаходить вихід з печери")
+# elif user_act == "3":
+#     print("Гравець потрапляє в пастку (лабіринт)")
+# else:
+#     print("Спробуй ще")
+
+   
