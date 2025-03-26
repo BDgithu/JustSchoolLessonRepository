991902c2a24fc196db3473e91f1973a268609eef
BD <107256942+BDgithu@users.noreply.github.com>
2025-03-26 18:50:04 +0200

    Add files via upload

---
+++ Sobaka dz.py
@@ -0,0 +1,5 @@
+imennyk = input("Введи іменник: ")
+dieslovo = input("Введи дієслово: ")
+prikmetnik = input("Введи прикметник: ")
+
+print(f"Одного разу {prikmetnik} {imennyk} вирішив {dieslovo} через паркан.")
