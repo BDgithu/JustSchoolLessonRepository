c75145cda01ebdc3c96c1fe28c4041ffc6e5ee41
BD <107256942+BDgithu@users.noreply.github.com>
2025-03-26 18:49:49 +0200

    Add files via upload

---
+++ Yrok 3.py
@@ -0,0 +1,26 @@
+#user_num = input("Please enter some integer number: ") #
+#sum = 0
+
+# while sum < 4: # 
+#     print(sum < 4)
+#     sum += 1
+# print(sum < 4)
+
+# rng = range(4)
+#(type(user_num))
+#for num in range(1, int(user_num) + 1): # 1 + 2 + 3 + 4 + 5
+#    print("num: ", num)
+#    sum += num
+
+#print("sum: ", sum)
+
+#res = 1
+#for i in range(2, int(user_num) + 1, 2):
+  #  print("i: ", i)
+   # res *= i
+#print (res)
+
+name = input ('Name')
+age = input ('Age')
+color = input ('Color')
+print (f"Name is {name}, Your color is {color}, Age {age}")
